25a8b04340ca8c1c8c2b9056f0a5f106348ac6f8
Alexander Ackermann <abackermann91@gmail.com>
2026-01-22 19:12:18 +0100

    fix: don't show version drop on landing page

---
+++ src/theme/NavbarItem/DocsVersionDropdownNavbarItem.tsx
@@ -0,0 +1,23 @@
+import React, {type ReactNode} from 'react';
+import DocsVersionDropdownNavbarItem from '@theme-original/NavbarItem/DocsVersionDropdownNavbarItem';
+import type DocsVersionDropdownNavbarItemType from '@theme/NavbarItem/DocsVersionDropdownNavbarItem';
+import type {WrapperProps} from '@docusaurus/types';
+import {useLocation} from '@docusaurus/router';
+
+type Props = WrapperProps<typeof DocsVersionDropdownNavbarItemType>;
+
+export default function DocsVersionDropdownNavbarItemWrapper(props: Props): ReactNode {
+  const location = useLocation();
+  const isDocsPage = location.pathname.split('/').includes('docs')
+
+  // Don't render version dropdown on landing pages
+  if (!isDocsPage) {
+    return null;
+  }
+
+  return (
+    <>
+      <DocsVersionDropdownNavbarItem {...props} />
+    </>
+  );
+}
